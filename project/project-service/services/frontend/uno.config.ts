d32ac6e21770c2dbe7c8fd60c786ef6fda7133d4
Azur冰弦 <973562770@qq.com>
2024-05-14 15:30:37 +0800

    chore(frontend): added unocss

---
+++ project/project-service/services/frontend/uno.config.ts
@@ -0,0 +1,10 @@
+// uno.config.ts
+import { defineConfig } from 'unocss'
+import { presetUno } from 'unocss'
+
+export default defineConfig({
+  // ...UnoCSS options
+  presets: [
+    presetUno()
+  ]
+})
